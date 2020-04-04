df8bf9ad8924728f4c16648d15aa67f526bba5a2
Jānis Tulišs <janistuliss3@gmail.com>
2020-04-04 09:43:09 +0300

    v.9.43

---
+++ Click The Button/ClickTheButton.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Click_The_Button
+{
+    partial class ClickTheButton
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClick = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tinyModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelClicks = new System.Windows.Forms.Label();
+            this.normalModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lightModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonClick
+            // 
+            this.buttonClick.Location = new System.Drawing.Point(156, 137);
+            this.buttonClick.Name = "buttonClick";
+            this.buttonClick.Size = new System.Drawing.Size(41, 41);
+            this.buttonClick.TabIndex = 0;
+            this.buttonClick.Text = "Click!";
+            this.buttonClick.UseVisualStyleBackColor = true;
+            this.buttonClick.Click += new System.EventHandler(this.buttonClick_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(438, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.darkModeToolStripMenuItem,
+            this.lightModeToolStripMenuItem,
+            this.tinyModeToolStripMenuItem,
+            this.normalModeToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // darkModeToolStripMenuItem
+            // 
+            this.darkModeToolStripMenuItem.Name = "darkModeToolStripMenuItem";
+            this.darkModeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.darkModeToolStripMenuItem.Text = "Dark Mode";
+            this.darkModeToolStripMenuItem.Click += new System.EventHandler(this.darkModeToolStripMenuItem_Click);
+            // 
+            // tinyModeToolStripMenuItem
+            // 
+            this.tinyModeToolStripMenuItem.Name = "tinyModeToolStripMenuItem";
+            this.tinyModeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tinyModeToolStripMenuItem.Text = "Tiny Mode";
+            this.tinyModeToolStripMenuItem.Click += new System.EventHandler(this.tinyModeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // labelClicks
+            // 
+            this.labelClicks.AutoSize = true;
+            this.labelClicks.Location = new System.Drawing.Point(13, 28);
+            this.labelClicks.Name = "labelClicks";
+            this.labelClicks.Size = new System.Drawing.Size(16, 13);
+            this.labelClicks.TabIndex = 2;
+            this.labelClicks.Text = "...";
+            // 
+            // normalModeToolStripMenuItem
+            // 
+            this.normalModeToolStripMenuItem.Name = "normalModeToolStripMenuItem";
+            this.normalModeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.normalModeToolStripMenuItem.Text = "Normal Mode";
+            this.normalModeToolStripMenuItem.Click += new System.EventHandler(this.normalModeToolStripMenuItem_Click);
+            // 
+            // lightModeToolStripMenuItem
+            // 
+            this.lightModeToolStripMenuItem.Name = "lightModeToolStripMenuItem";
+            this.lightModeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lightModeToolStripMenuItem.Text = "Light Mode";
+            this.lightModeToolStripMenuItem.Click += new System.EventHandler(this.lightModeToolStripMenuItem_Click);
+            // 
+            // ClickTheButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 428);
+            this.Controls.Add(this.labelClicks);
+            this.Controls.Add(this.buttonClick);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ClickTheButton";
+            this.Text = "Click The Button";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClick;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem darkModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tinyModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label labelClicks;
+        private System.Windows.Forms.ToolStripMenuItem normalModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lightModeToolStripMenuItem;
+    }
+}
+
---
+++ Click The Button/ClickTheButton.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Click_The_Button
+{
+    
+    public partial class ClickTheButton : Form
+    {
+        Random rand = new Random();
+        private PictureBox zone;        
+        int clicks = 0;
+
+        public ClickTheButton()
+        {
+            InitializeComponent();
+            InitializeGameBorder();
+            UpdateClicks();
+        }       
+
+        private void buttonClick_Click(object sender, EventArgs e)
+        {                                  
+            buttonClick.Left = rand.Next(50, 350);
+            buttonClick.Top = rand.Next(50, 350);           
+            clicks++;
+            UpdateClicks();            
+        }
+        private void InitializeGameBorder()
+        {
+            zone = new PictureBox();
+            zone.Left = 50;
+            zone.Top = 50;
+            zone.Width = 350;
+            zone.Height = 350;
+            zone.BackColor = Color.White;
+            this.Controls.Add(zone);
+        }       
+
+        private void UpdateClicks()
+        {
+            labelClicks.Text = "Click's: " + clicks.ToString();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();        
+        }
+
+        private void tinyModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {            
+            buttonClick.Width = 10;
+            buttonClick.Height = 10;
+            clicks = 0;
+            UpdateClicks();
+        }
+
+        private void normalModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            buttonClick.Width = 41;
+            buttonClick.Height = 41;
+            clicks = 0;
+            UpdateClicks();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Don't Waste Your Time go CLICK!");
+        }
+
+        private void darkModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Color darkColor = Color.Gray;
+            Color lightColor = Color.Black;
+            buttonClick.BackColor = darkColor;
+            buttonClick.ForeColor = lightColor;
+            menuStrip1.BackColor = darkColor;
+            menuStrip1.ForeColor = lightColor;
+            this.BackColor = darkColor;
+            this.ForeColor = lightColor;
+            zone.BackColor = Color.LightGray;
+        }
+
+        private void lightModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Color darkColor = Color.Black;
+            Color lightColor = Color.WhiteSmoke;
+            buttonClick.BackColor = lightColor;
+            buttonClick.ForeColor = darkColor;
+            menuStrip1.BackColor = lightColor;
+            menuStrip1.ForeColor = darkColor;
+            this.BackColor = lightColor;
+            this.ForeColor = darkColor;
+            zone.BackColor = Color.White;
+        }
+
+       
+    }
+}
